81113686e2798bbe7c981129a687c1f4c6cfee00
hann-zhong <50278606+hann-zhong@users.noreply.github.com>
2019-10-01 18:21:24 -0700

    Update SpookAction.cs

--- Assets/Scripts/SpookAction.cs
+++ Assets/Scripts/SpookAction.cs
@@ -9,11 +9,13 @@ public class SpookAction : MonoBehaviour
 
     void Start()
     {
+        //Wizard is tagged as player in Unity, then set as the target
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
+        //Spooks move towards position of the target
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
