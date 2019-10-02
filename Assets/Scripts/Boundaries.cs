9e1f115b775efcb1ebadbdb21d46b6767206f0b0
hann-zhong <50278606+hann-zhong@users.noreply.github.com>
2019-10-01 18:22:20 -0700

    Update Boundaries.cs

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -7,6 +7,7 @@ public class Boundaries : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Prevents player and ghosts from moving out of the frame
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7f, 7f), Mathf.Clamp(transform.position.y, -2f, 2f), transform.position.z);
 
     }
