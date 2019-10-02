a06b5563dbf9685188dea8eeb6136f9367a9559c
hann-zhong <50278606+hann-zhong@users.noreply.github.com>
2019-10-01 18:24:32 -0700

    Update Restart.cs

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -7,6 +7,7 @@ public class Restart : MonoBehaviour
 {
     public void restartScene()
     {
+        //When the restart button is pressed, beginning scene will be loaded and the score counter will reset to 0
         SceneManager.LoadScene("SampleScene");
         ScoreCounter.scoreVal = 0;
     }
