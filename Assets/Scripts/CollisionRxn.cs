faeb29bd44dbe39ac609fa6f725b0b2e89f8e890
hann-zhong <50278606+hann-zhong@users.noreply.github.com>
2019-10-01 18:23:38 -0700

    Update CollisionRxn.cs

--- Assets/Scripts/CollisionRxn.cs
+++ Assets/Scripts/CollisionRxn.cs
@@ -7,6 +7,7 @@ public class CollisionRxn : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //When fireball collides with something, the fireball and the object it collides with disappears
         ScoreCounter.scoreVal += 10;
         Destroy(gameObject);
         Destroy(collision.gameObject);
